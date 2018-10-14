8308d898456c882919f5a818069c988366caea55
Alexey Khrusch <alexeykhr@outlook.com>
2018-10-14 13:27:38 +0300

    Add basic app.js

---
+++ app.js
@@ -0,0 +1,52 @@
+'use strict'
+
+// const request = require('request')
+//
+// request('http://google.com', (err, resp, body) => {
+//   console.log('error:', err)
+//   console.log('statusCode:', resp)
+// })
+//
+// const socket = require('socket.io-client')('http://localhost:3000')
+//
+// socket.on('connect', () => {
+//   console.log('connect')
+// })
+//
+// socket.on('disconnect', () => {
+//   console.log('disconnect')
+// })
+//
+// setInterval(() => {
+//   socket.emit('event', 'data')
+// }, 2000)
+
+// const si = require('systeminformation')
+//
+// si.getAllData()
+//   .then(cb => console.log(cb))
+//   .catch(error => console.error(error))
+
+// console.log(si.time())
+//
+// console.log('-----------------')
+//
+// si.cpu()
+//   .then(data => { console.log(data); console.log('-----------------') })
+//   .catch(error => console.error(error))
+//
+// si.mem()
+//   .then(data => { console.log(data); console.log('-----------------') })
+//   .catch(error => console.error(error))
+//
+// si.graphics()
+//   .then(data => { console.log(data); console.log('-----------------') })
+//   .catch(error => console.error(error))
+//
+// si.osInfo()
+//   .then(data => { console.log(data); console.log('-----------------') })
+//   .catch(error => console.error(error))
+//
+// si.networkInterfaces()
+//   .then(data => { console.log(data); console.log('-----------------') })
+//   .catch(error => console.error(error))
